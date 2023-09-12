3e5bc61504dbc82922ce966d3ea79ecc5699b8a4
KristianTrifork <krkr@trifork.com>
2023-09-12 15:30:17 +0200

    made a diagram for the job

--- MultipleSideOutput/ComponentTest/ComponentTest.cs
+++ MultipleSideOutput/ComponentTest/ComponentTest.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Xunit;
 using MultipleSideOutputExample.ComponentTest.Models;
+using Confluent.Kafka;
 
 namespace MultipleSideOutputExample.ComponentTest;
 
@@ -29,7 +30,7 @@ public class ComponentTest
             .Build();
     }
 
-    [Fact]
+    //[Fact]
     public void Should_BeImplemented_When_ServiceIsCreated()
     {
         // Arrange
@@ -70,10 +71,73 @@ public class ComponentTest
         messages.Should().ContainSingle(message => 
             message.DeviceId == inputEvent.DeviceId && 
             message.Value == inputEvent.ValueA &&
-            message.Timestamp == inputEvent.Timestamp &&
-            message.ExtraField == "ExtraFieldValue");
+            message.Timestamp == inputEvent.Timestamp);
         reader.VerifyNoMoreMessages(TimeSpan.FromSeconds(20)).Should().BeTrue();
 
         Assert.True(false, "This is really just here to make the test fail and ensure that you either decide to implement a component test or actively decide not to");
+    }
+
+    [Fact]
+    public void Test_First_Message_On_All_Topics(){
+        // Arrange
+        // Setting up the writer (Kafka producer), not using a message key.
+        var writer = KafkaWriterBuilder.Create<string, MultipleSideOutputExampleInputEvent>(_configuration)
+            .WithTopic("MultipleSideOutputExampleInputTopic")
+            .WithKeyFunction(model => model.DeviceId)
+            .Build();
+
+        // Setting up readers (Kafka consumer)
+        var reader_topicA = KafkaReaderBuilder.Create<string, MultipleSideOutputExampleOutputEvent>(_configuration)
+            .WithTopic("OutputA-events")
+            .WithGroupId("ComponentTest")
+            .Build();
+
+        var reader_topicB = KafkaReaderBuilder.Create<string, MultipleSideOutputExampleOutputEvent>(_configuration)
+            .WithTopic("OutputB-events")
+            .WithGroupId("ComponentTest")
+            .Build();
+
+        var reader_topicCD = KafkaReaderBuilder.Create<string, MultipleSideOutputExampleOutputEvent2>(_configuration)
+            .WithTopic("OutputCD-events")
+            .WithGroupId("ComponentTest")
+            .Build();
+
+        // Act
+        // Making an input event
+        var inputEvent = new MultipleSideOutputExampleInputEvent(){
+            DeviceId = "Test_First_Message_On_All_Topics",
+            ValueA = 100,
+            ValueB = 100,
+            ValueC = 100,
+            ValueD = 100,
+            Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds()
+        };
+
+        writer.Write(inputEvent);
+
+        // Assert
+        // Assuming the job produce the message on each topic
+        var messages_topicA = reader_topicA.ReadMessages(1, TimeSpan.FromSeconds(20));
+        var messages_topicB = reader_topicB.ReadMessages(1, TimeSpan.FromSeconds(20));
+        var messages_topicCD = reader_topicCD.ReadMessages(1, TimeSpan.FromSeconds(20));
+
+        messages_topicA.Should().ContainSingle(message =>
+            message.DeviceId == inputEvent.DeviceId &&
+            message.Value == inputEvent.ValueA &&
+            message.Timestamp == inputEvent.Timestamp
+        );
+
+        messages_topicB.Should().ContainSingle(message =>
+            message.DeviceId == inputEvent.DeviceId &&
+            message.Value == inputEvent.ValueB &&
+            message.Timestamp == inputEvent.Timestamp
+        );
+
+        messages_topicCD.Should().ContainSingle(message =>
+            message.DeviceId == inputEvent.DeviceId &&
+            message.ValueC == inputEvent.ValueC &&
+            message.ValueD == inputEvent.ValueD &&
+            message.Timestamp == inputEvent.Timestamp
+        );
     }
 }
--- MultipleSideOutput/ComponentTest/Models/MultipleSideOutputExampleOutputEvent2.cs
+++ MultipleSideOutput/ComponentTest/Models/MultipleSideOutputExampleOutputEvent2.cs
@@ -1,15 +1,18 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
 
 namespace MultipleSideOutputExample.ComponentTest.Models;
 
 // Rename and implement a model that fits the one you expect your job to produce
-public class MultipleSideOutputExampleOutputEvent
+public class MultipleSideOutputExampleOutputEvent2
 {
     [JsonPropertyName("deviceId")]
     public string DeviceId { get; set; }
 
-    [JsonPropertyName("value")]
-    public double Value { get; set; }
+    [JsonPropertyName("valueC")]
+    public double ValueC { get; set; }
+
+    [JsonPropertyName("valueD")]
+    public double ValueD { get; set; }
 
     [JsonPropertyName("timestamp")]
     public long Timestamp { get; set; }
