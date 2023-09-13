6a0c9bfe13921252299e57040ed2cd100a4889c4
KristianTrifork <krkr@trifork.com>
2023-09-13 14:25:59 +0200

    componentest working, missing Unit test

--- MultipleSideOutput/ComponentTest/ComponentTest.cs
+++ MultipleSideOutput/ComponentTest/ComponentTest.cs
@@ -30,55 +30,8 @@ public class ComponentTest
             .Build();
     }
 
-    //[Fact]
-    public void Should_BeImplemented_When_ServiceIsCreated()
-    {
-        // Arrange
-        // Here you'll set up one or more writers and readers, which connect to the topic(s) that your job consumes
-        // from and publishes to. 
-        var writer = KafkaWriterBuilder.Create<string, MultipleSideOutputExampleInputEvent>(_configuration)
-            .WithTopic("MultipleSideOutputExampleInputTopic") // The topic to consume from
-            .WithKeyFunction(model => model.DeviceId) // Optional function to retrieve the message key.
-                                                          // If no key is desired, use KafkaWriterBuilder.Create<Null, InputModel>
-                                                          // and make this function return null
-            .Build();
-
-        var reader = KafkaReaderBuilder.Create<string, MultipleSideOutputExampleOutputEvent>(_configuration)
-            .WithTopic("MultipleSideOutputExampleOutputTopic")
-            .WithGroupId("MyGroup")
-            .Build();
-        
-        // Act
-        // Write one or more messages to the writer
-        var inputEvent = new MultipleSideOutputExampleInputEvent()
-        {
-            DeviceId = "deviceId-1",
-            ValueA = 12.34,
-            ValueB = 22.34,
-            ValueC = 32.34,
-            ValueD = 42.34,
-            Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds()
-        };
-        
-        writer.Write(inputEvent);
-        
-        // Assert
-        // Then consume using the reader, supplying how many output messages your input messages expected to generate
-        // as well as the maximum duration it is allowed to take for those messages to be produced
-        var messages = reader.ReadMessages(1, TimeSpan.FromSeconds(20));
-        
-        // Then evaluate whether your messages are as expected, and that there are only as many as you expected 
-        messages.Should().ContainSingle(message => 
-            message.DeviceId == inputEvent.DeviceId && 
-            message.Value == inputEvent.ValueA &&
-            message.Timestamp == inputEvent.Timestamp);
-        reader.VerifyNoMoreMessages(TimeSpan.FromSeconds(20)).Should().BeTrue();
-
-        Assert.True(false, "This is really just here to make the test fail and ensure that you either decide to implement a component test or actively decide not to");
-    }
-
     [Fact]
-    public void Test_First_Message_On_All_Topics(){
+    public void TestFirstMessageOnAllTopics(){
         // Arrange
         // Setting up the writer (Kafka producer), not using a message key.
         var writer = KafkaWriterBuilder.Create<string, MultipleSideOutputExampleInputEvent>(_configuration)
@@ -105,7 +58,7 @@ public class ComponentTest
         // Act
         // Making an input event
         var inputEvent = new MultipleSideOutputExampleInputEvent(){
-            DeviceId = "Test_First_Message_On_All_Topics",
+            DeviceId = "ComponentTest",
             ValueA = 100,
             ValueB = 100,
             ValueC = 100,
@@ -126,12 +79,14 @@ public class ComponentTest
             message.Value == inputEvent.ValueA &&
             message.Timestamp == inputEvent.Timestamp
         );
+        reader_topicA.VerifyNoMoreMessages(TimeSpan.FromSeconds(20)).Should().BeTrue();
 
         messages_topicB.Should().ContainSingle(message =>
             message.DeviceId == inputEvent.DeviceId &&
             message.Value == inputEvent.ValueB &&
             message.Timestamp == inputEvent.Timestamp
         );
+        reader_topicB.VerifyNoMoreMessages(TimeSpan.FromSeconds(20)).Should().BeTrue();
 
         messages_topicCD.Should().ContainSingle(message =>
             message.DeviceId == inputEvent.DeviceId &&
@@ -139,5 +94,7 @@ public class ComponentTest
             message.ValueD == inputEvent.ValueD &&
             message.Timestamp == inputEvent.Timestamp
         );
+        reader_topicCD.VerifyNoMoreMessages(TimeSpan.FromSeconds(20)).Should().BeTrue();
     }
+    
 }
