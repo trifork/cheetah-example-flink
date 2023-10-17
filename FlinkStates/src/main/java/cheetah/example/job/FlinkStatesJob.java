359dbe5e4ab2e36997f11dd8062e7aa65ef679b1
Magnus Nørgaard <mnr@trifork.com>
2023-10-17 12:02:55 +0200

    Update FlinkStates lib processing to 3.3.2

--- FlinkStates/src/main/java/cheetah/example/job/FlinkStatesJob.java
+++ FlinkStates/src/main/java/cheetah/example/job/FlinkStatesJob.java
@@ -2,12 +2,13 @@ package cheetah.example.job;
 
 import cheetah.example.function.*;
 import cheetah.example.model.InputEvent;
-import com.trifork.cheetah.processing.connector.kafka.KafkaDataStreamBuilder;
-import com.trifork.cheetah.processing.connector.kafka.KafkaSinkBuilder;
+import com.trifork.cheetah.processing.connector.kafka.CheetahKafkaSource;
+import com.trifork.cheetah.processing.connector.kafka.config.CheetahKafkaSinkConfig;
+import com.trifork.cheetah.processing.connector.kafka.config.CheetahKafkaSourceConfig;
 import com.trifork.cheetah.processing.job.Job;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 
@@ -25,30 +26,25 @@ public class FlinkStatesJob extends Job implements Serializable {
 
     @Override
     protected void setup() {
+
         // Input source
-        final DataStream<InputEvent> inputStream =
-                KafkaDataStreamBuilder.forSource(this, InputEvent.class)
-                        .build();
+        final KafkaSource<InputEvent> kafkaSource = CheetahKafkaSourceConfig.builder(this).toKafkaSourceBuilder(InputEvent.class).build();
+        final KeyedStream<InputEvent, String> keyedByStream = CheetahKafkaSource.toDataStream(this, kafkaSource, "Event Input Source")
+                .keyBy(InputEvent::getDeviceId);
 
-        final KeyedStream<InputEvent, String> keyedByStream = inputStream.keyBy(InputEvent::getDeviceId);
-
-        mapAndSink(keyedByStream, new FlinkValueStatesMapper(), Double.class, "-value");
-        mapAndSink(keyedByStream, new FlinkReducingStatesMapper(), Double.class, "-reducing");
-        mapAndSink(keyedByStream, new FlinkAggregatingStatesMapper(), Double.class, "-aggregating");
-        mapAndSink(keyedByStream, new FlinkMapStatesMapper(), Double.class, "-map");
-        mapAndSink(keyedByStream, new FlinkListStatesMapper(), Double[].class, "-list");
+        // Setup each mapper and corresponding sink
+        mapAndSink(keyedByStream, new FlinkValueStatesMapper(), Double.class, "value");
+        mapAndSink(keyedByStream, new FlinkReducingStatesMapper(), Double.class, "reducing");
+        mapAndSink(keyedByStream, new FlinkAggregatingStatesMapper(), Double.class, "aggregating");
+        mapAndSink(keyedByStream, new FlinkMapStatesMapper(), Double.class, "map");
+        mapAndSink(keyedByStream, new FlinkListStatesMapper(), Double[].class, "list");
     }
 
     public <T> void mapAndSink(KeyedStream<InputEvent, String> keyedStream, RichFlatMapFunction<InputEvent, T> function, Class<T> outputType, String kafkaPostFix){
 
-        final SingleOutputStreamOperator<T> listOutputStream =
-                keyedStream.flatMap(function);
+        final SingleOutputStreamOperator<T> outputStream = keyedStream.flatMap(function);
+        final KafkaSink<T> sink = CheetahKafkaSinkConfig.builder(this, kafkaPostFix).toSinkBuilder(outputType).build();
 
-        final KafkaSink<T> listSink =
-                KafkaSinkBuilder.defaultKafkaConfig(this, outputType)
-                        .kafkaPostfix(kafkaPostFix)
-                        .build();
-
-        listOutputStream.sinkTo(listSink);
+        outputStream.sinkTo(sink);
     }
 }
