12918dc7dba2ecdf1d814b5f5ad857f29fd11f7f
Daniel Tøttrup <dat@trifork.com>
2024-01-10 10:25:33 +0100

    Update TumblingWindow example to new dotnet package and keycloak

--- TumblingWindow/ComponentTest/ComponentTest.cs
+++ TumblingWindow/ComponentTest/ComponentTest.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
-using Cheetah.ComponentTest.Kafka;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Xunit;
 using TumblingWindow.ComponentTest.Models;
 using System.Linq;
 using System.Threading.Tasks;
+using Cheetah.Kafka.Testing;
 
 namespace TumblingWindow.ComponentTest;
 
@@ -20,10 +20,11 @@ public class ComponentTest
         // These will be overriden by environment variables from compose
         var conf = new Dictionary<string, string>()
         {
-            {"KAFKA:AUTHENDPOINT", "http://localhost:1752/oauth2/token"},
-            {"KAFKA:CLIENTID", "ClientId" },
-            {"KAFKA:CLIENTSECRET", "1234" },
-            {"KAFKA:URL", "localhost:9092"}
+            { "KAFKA:URL", "localhost:9092" },
+            { "KAFKA:OAUTH2:CLIENTID", "default-access" },
+            { "KAFKA:OAUTH2:CLIENTSECRET", "default-access-secret" },
+            { "KAFKA:OAUTH2:SCOPE", "kafka" },
+            { "KAFKA:OAUTH2:TOKENENDPOINT", "http://localhost:1852/realms/local-development/protocol/openid-connect/token" }
         };
         _configuration = new ConfigurationBuilder()
             .AddInMemoryCollection(conf)
@@ -35,22 +36,14 @@ public class ComponentTest
     public async Task Tumbling_Window_Component_Test()
     {
         // Arrange
-        // Here you'll set up one or more writers and readers, which connect to the topic(s) that your job consumes
-        // from and publishes to. 
-        var writer = KafkaWriterBuilder.Create<string, InputEvent>(_configuration)
-            .WithTopic("TumblingWindowInputTopic") // The topic to consume from
-            .WithKeyFunction(model => model.DeviceId) // Optional function to retrieve the message key.
-                                                          // If no key is desired, use KafkaWriterBuilder.Create<Null, InputModel>
-                                                          // and make this function return null
-            .Build();
-
-        var reader = KafkaReaderBuilder.Create<string, Models.EventWindow>(_configuration)
-            .WithTopic("TumblingWindowOutputTopic")
-            .WithConsumerGroup("MyGroup")
-            .Build();
+        // Create a KafkaTestClientFactory to create KafkaTestReaders and KafkaTestWriters
+        var kafkaClientFactory = KafkaTestClientFactory.Create(_configuration);
+        
+        var writer = kafkaClientFactory.CreateTestWriter<InputEvent>("TumblingWindowInputTopic");
+        var reader = kafkaClientFactory.CreateTestReader<EventWindow>("TumblingWindowOutputTopic");
         
         // Act
-        // Write one or more messages to the writer
+        // Write messages to the writer
         var inputEvent1 = new InputEvent()
         {
             DeviceId = "deviceId-1",
@@ -76,26 +69,22 @@ public class ComponentTest
             Value = 910.1112,
             Timestamp = DateTimeOffset.Now.AddMinutes(10).ToUnixTimeMilliseconds()
         };
-
         
         await writer.WriteAsync(inputEvent1, inputEvent2, inputEvent3);
-
         await writer.WriteAsync(inputEvent4, inputEvent4);
-
         
         // Assert
-        // Then consume using the reader, supplying how many output messages your input messages expected to generate
-        // as well as the maximum duration it is allowed to take for those messages to be produced
+        // Read messages from the reader
         var messages = reader.ReadMessages(1, TimeSpan.FromSeconds(20));
         
-        // Then evaluate whether your messages are as expected, and that there are only as many as you expected 
+        // Evaluate the messages 
         messages.Should().ContainSingle(message => 
             message.DeviceId == inputEvent1.DeviceId &&
             message.Values.Count == 3 &&
             message.Values.All(item => new double[]{inputEvent1.Value,
-                                                    inputEvent2.Value,
-                                                    inputEvent3.Value}
-                                                    .Contains(item)));
+                    inputEvent2.Value,
+                    inputEvent3.Value}
+                .Contains(item)));
         reader.VerifyNoMoreMessages(TimeSpan.FromSeconds(20)).Should().BeTrue();
     }
 
