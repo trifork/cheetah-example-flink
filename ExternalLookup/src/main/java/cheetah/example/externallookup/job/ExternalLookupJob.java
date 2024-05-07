caa38498563ebf455adc0ee5aaa2608782341f07
Rolf Nordestgaard <rno@trifork.com>
2024-05-07 13:53:41 +0200

    Created ConfigProvider class and implemented use of ParamHelper to get parameters

--- ExternalLookup/src/main/java/cheetah/example/externallookup/job/ExternalLookupJob.java
+++ ExternalLookup/src/main/java/cheetah/example/externallookup/job/ExternalLookupJob.java
@@ -1,5 +1,6 @@
 package cheetah.example.externallookup.job;
 
+import cheetah.example.externallookup.config.ConfigProvider;
 import cheetah.example.externallookup.function.ExternalLookupMapper;
 import cheetah.example.externallookup.model.InputEvent;
 import cheetah.example.externallookup.model.OutputEvent;
@@ -7,7 +8,6 @@ import com.trifork.cheetah.processing.connector.kafka.CheetahKafkaSource;
 import com.trifork.cheetah.processing.connector.kafka.config.CheetahKafkaSinkConfig;
 import com.trifork.cheetah.processing.connector.kafka.config.CheetahKafkaSourceConfig;
 import com.trifork.cheetah.processing.job.Job;
-import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
@@ -16,7 +16,6 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -40,12 +39,14 @@ public class ExternalLookupJob extends Job implements Serializable {
 
         final DataStream<InputEvent> inputStream = CheetahKafkaSource.toDataStream(this, kafkaSource, "Event Input Source", "Event Input Source");
 
+        final ConfigProvider configProvider = new ConfigProvider(this);
+
         // Get configuration from ENV
-        final String idServiceHost = Objects.requireNonNull(System.getenv("ID_SERVICE_URL"), "ID_SERVICE_URL required");
-        final String tokenUrl = Objects.requireNonNull(System.getenv("ID_SERVICE_TOKEN_URL"), "ID_SERVICE_TOKEN_URL required");
-        final String clientId = Objects.requireNonNull(System.getenv("ID_SERVICE_CLIENT_ID"), "ID_SERVICE_CLIENT_ID required");
-        final String clientSecret = Objects.requireNonNull(System.getenv("ID_SERVICE_CLIENT_SECRET"), "ID_SERVICE_CLIENT_SECRET required");
-        final String scope = Objects.requireNonNull(System.getenv("ID_SERVICE_SCOPE"), "ID_SERVICE_SCOPE required");
+        final String idServiceHost = configProvider.getIdServiceUrl();
+        final String tokenUrl = configProvider.getIdServiceToken();
+        final String clientId = configProvider.getIdServiceClientId();
+        final String clientSecret = configProvider.getIdServiceClientSecret();
+        final String scope = configProvider.getIdServiceScope();
 
         // Transform stream
         final SingleOutputStreamOperator<OutputEvent> outputStream =
